0983dc6a22d49def9375d7d7395bfae2df80a88b
Daniel Matthew <hello@danmatthew.co.uk>
2015-09-12 15:57:02 +0100

    POST and GET routes for beer

--- server.js
+++ server.js
@@ -1,3 +1,65 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var firebase = require('firebase');
+
 
 var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  res.json({
+    'message': 'No beers'
+  });
+});
+
+var beersRoute = router.route('/beers');
+
+beersRoute.post(function(req, res) {
+  var beersRef = new firebase('https://beers-dm.firebaseio.com/beers');
+
+  var newBeerRef = beersRef.push();
+  newBeerRef.set({
+    name: req.body.name,
+    style: req.body.style
+  }, function(error) {
+    if (error) {
+      res.send(err);
+    } else {
+      res.json({
+        'message': 'Saved successfully: ' + newBeerRef.key()
+      });
+    }
+  });
+});
+
+beersRoute.get(function(req, res) {
+  var beersRef = new firebase('https://beers-dm.firebaseio.com/beers');
+
+  beersRef.once('value', function(snapshot) {
+    res.json(snapshot.val());
+  }, function(errorObject) {
+    res.send(errorObject);
+  });
+});
+
+var beer = router.route('/beers/:id');
+beer.get(function(req, res) {
+  var beersRef = new firebase('https://beers-dm.firebaseio.com/beers');
+
+  beersRef.once('value', function(snapshot) {
+    res.json(snapshot.val());
+  }, function(errorObject) {
+    res.send(errorObject);
+  });
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Beers on port ' + port);
