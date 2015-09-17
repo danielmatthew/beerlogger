71c34bedea58c4a7ca4bed9d9b864f60ad8e425e
Daniel Matthew <hello@danmatthew.co.uk>
2015-09-17 21:05:03 +0100

    Refactored app.js

--- src/public/app.js
+++ src/public/app.js
@@ -1,5 +1,34 @@
-angular.module('beerApp', [
-  'routes',
-  'mainCtrl',
-  'BeerService'
-]);
+angular.module('beerApp', ['ngRoute', 'beers.controllers'])
+  .config(function($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'src/public/views/beers.html',
+        controller: 'BeerCtrl'
+      });
+
+    $locationProvider.html5Mode(true);
+  });
+
+angular.module('beers.controllers', ['beers.services'])
+  .controller('BeerCtrl', function($scope) {
+    console.log('yo');
+  });
+
+angular.module('beers.services', [])
+  .factory('Beer', function($http) {
+    var o = {};
+
+    o.all = function() {
+      return $http.get('/api/beers');
+    };
+
+    o.get = function(id) {
+      return $http.get('/api/beers' + id);
+    };
+
+    o.delete = function(id) {
+      return $http.delete('/api/beers/' + id);
+    };
+
+    return o;
+  });
